fb9ece64a80a765650c31e1effccce799857d415
mahhov1 <mahhov1@gmail.com>
2022-07-03 19:13:26 -0700

    clean main

--- src/util.js
+++ src/util.js
@@ -2,12 +2,15 @@ import * as THREE from 'three';
 
 const UP = new THREE.Vector3(0, 1, 0);
 
-const clamp = (v, min, max) => Math.min(Math.max(v, min), max);
-const rand = max => Math.random() * max;
-const radian = degree => degree / 180 * Math.PI;
+let sleep = ms => new Promise(r => setTimeout(r, ms));
+
+let clamp = (v, min, max) => Math.min(Math.max(v, min), max);
+let rand = max => Math.random() * max;
+let radian = degree => degree / 180 * Math.PI;
 
 export {
 	UP,
+	sleep,
 	clamp,
 	rand,
 	radian,
