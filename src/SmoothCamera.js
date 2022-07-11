5dc8e6e0782fa3623b05b6aeb588360d07c1df08
mahhov1 <mahhov1@gmail.com>
2022-07-11 10:16:18 -0700

    smooth camera average

--- src/SmoothCamera.js
+++ src/SmoothCamera.js
@@ -2,26 +2,34 @@ import * as THREE from 'three';
 
 class SmoothCamera {
 	#camera;
+	#followPosition = null;
 
 	constructor(camera) {
 		this.#camera = camera;
 	}
 
-	reset(){
-		this.#camera.position.copy(0, 0, -1000);
+	reset() {
+		this.#followPosition = null;
 	}
 
 	follow(position) {
-		const DISTANCE = 25;
+		const WEIGHT = .7;
+		const DISTANCE = 15;
 		const HEIGHT = 15;
 
-		let delta = position.clone().sub(this.#camera.position);
+		if (!this.#followPosition) {
+			this.#followPosition = position;
+			this.#camera.position.copy(position);
+		} else
+			this.#followPosition.multiplyScalar(WEIGHT).addScaledVector(position, 1 - WEIGHT);
+
+		let delta = this.#followPosition.clone().sub(this.#camera.position);
 		delta.y = 0;
 		if (!delta.length())
 			delta = new THREE.Vector3(0, 0, 1);
 		delta.setLength(DISTANCE);
-		this.#camera.position.copy(position.clone().sub(delta));
-		this.#camera.position.y += HEIGHT
+		this.#camera.position.copy(this.#followPosition.clone().sub(delta));
+		this.#camera.position.y += HEIGHT;
 		this.#camera.lookAt(position);
 	}
 }
