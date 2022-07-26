99413fb4058ab33c8cfe0aa9f775bf9e08f369a0
mahhov1 <mahhov1@gmail.com>
2022-07-26 07:53:21 -0700

    redo segment creator with point connections and line extensions

--- src/track/SegmentCreator.js
+++ src/track/SegmentCreator.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import IntersectionManager from '../IntersectionManager.js';
 import {getBounds, radian, UP} from '../util/util.js';
 import Track from './Track.js';
 
@@ -8,86 +9,92 @@ class Segment {
 	left2;
 	right2;
 
-	left;
-	bottom;
-	normal;
-
 	constructor(left1, right1, left2, right2) {
 		this.left1 = left1;
 		this.right1 = right1;
 		this.left2 = left2;
 		this.right2 = right2;
-
-		this.left = this.subLeft1(this.left2);
-		this.bottom = this.subLeft1(this.right1);
-		this.normal = this.left.clone().cross(this.bottom).normalize();
 	}
 
-	static fromLine(p1, p2, w1, w2) {
-		const SMOOTHNESS = 0;
+	static fromLine(p1, w1, p2, w2) {
 		let dir = p2.clone().sub(p1).normalize();
 		let dir90 = dir.clone().projectOnPlane(UP).applyAxisAngle(UP, radian(90));
-		let left1 = p1.clone().addScaledVector(dir, w1 * SMOOTHNESS).addScaledVector(dir90, w1);
-		let right1 = p1.clone().addScaledVector(dir, w1 * SMOOTHNESS).addScaledVector(dir90, -w1);
-		let left2 = p2.clone().addScaledVector(dir, -w2 * SMOOTHNESS).addScaledVector(dir90, w2);
-		let right2 = p2.clone().addScaledVector(dir, -w2 * SMOOTHNESS).addScaledVector(dir90, -w2);
+		let left1 = p1.clone().addScaledVector(dir90, w1);
+		let right1 = p1.clone().addScaledVector(dir90, -w1);
+		let left2 = p2.clone().addScaledVector(dir90, w2);
+		let right2 = p2.clone().addScaledVector(dir90, -w2);
 		return new Segment(left1, right1, left2, right2);
-	}
-
-	static connectSegments(segment1, segment2) {
-		return new Segment(segment1.left2, segment1.right2, segment2.left1, segment2.right1);
 	}
 
 	subLeft1(vec) {
 		return vec.clone().sub(this.left1);
 	}
+
+	subRight1(vec) {
+		return vec.clone().sub(this.right1);
+	}
+
+	get left() {
+		return this.subLeft1(this.left2);
+	}
+
+	get right() {
+		return this.subRight1(this.right2);
+	}
+
+	get bottom() {
+		return this.subLeft1(this.right1);
+	}
+
+	get normal() {
+		return this.left.clone().cross(this.bottom).normalize();
+	}
+
+	get flatLeftLine() {
+		return new THREE.Line3(IntersectionManager.flat(this.left1), IntersectionManager.flat(this.left2));
+	}
+
+	get flatRightLine() {
+		return new THREE.Line3(IntersectionManager.flat(this.right1), IntersectionManager.flat(this.right2));
+	}
 }
 
 class SegmentCreator {
-	#segments = [];
-	#position;
-	#width = 30;
+	#xyzws = [];
 
-	lineAt(x, z, x2, z2, width = this.#width) {
-		this.moveTo(x, 0, z, width);
-		this.pathTo(x2, 0, z2, width);
+	point(x, y, z, w = 30) {
+		this.#xyzws.push([x, y, z, w]);
 		return this;
-	}
-
-	vertLineAt(x, y, z, x2, y2, z2, width = this.#width) {
-		this.moveTo(x, y, z, width);
-		this.pathTo(x2, y2, z2, width);
-		return this;
-	}
-
-	moveTo(x, y, z, width = this.#width) {
-		this.#position = new THREE.Vector3(x, y, z);
-		this.#width = width;
-		return this;
-	}
-
-	pathTo(x, y, z, width = this.#width) {
-		let newSegment = Segment.fromLine(this.#position, new THREE.Vector3(x, y, z), this.#width, width);
-		let lastSegment = this.#segments[this.#segments.length - 1];
-		if (lastSegment)
-			this.#segments.push(Segment.connectSegments(lastSegment, newSegment));
-		this.#segments.push(newSegment);
-		return this.moveTo(x, y, z, width);
 	}
 
 	done() {
-		let lastSegment = this.#segments[this.#segments.length - 1];
-		if (lastSegment)
-			this.#segments.push(Segment.connectSegments(lastSegment, this.#segments[0]));
+		let segments = this.#xyzws
+			.map(xyzw => [new THREE.Vector3(...xyzw), xyzw[3]])
+			.map((p, i, a) => {
+				let j = (i + 1) % a.length;
+				return Segment.fromLine(...p, ...a[j]);
+			});
+		segments.forEach((segment, i, a) => {
+			let j = (i + 1) % a.length;
+			let nextSegment = a[j];
+			let leftIntersection = IntersectionManager.test2Lines(segment.flatLeftLine, nextSegment.flatLeftLine, true);
+			if (leftIntersection) {
+				segment.left2 = segment.left1.clone().addScaledVector(segment.left, leftIntersection.distance);
+				nextSegment.left1 = segment.left2.clone();
+			}
+			let rightIntersection = IntersectionManager.test2Lines(segment.flatRightLine, nextSegment.flatRightLine, true);
+			if (rightIntersection) {
+				segment.right2 = segment.right1.clone().addScaledVector(segment.right, rightIntersection.distance);
+				nextSegment.right1 = segment.right2.clone();
+			}
+		});
 
-		let bounds = [0, 0, 0].map((_, i) => getBounds(...this.#segments.flatMap(s =>
-			[s.left1.getComponent(i), s.right1.getComponent(i)])));
+		let bounds = [0, 0, 0].map((_, i) => getBounds(...segments.flatMap(s => [s.left1.getComponent(i), s.right1.getComponent(i)])));
 		let shift = new THREE.Vector3().fromArray(bounds.map(b => b[0])).multiplyScalar(-1).add(new THREE.Vector3(100, 0, 100));
 		let max = new THREE.Vector3().fromArray(bounds.map(b => b[1]));
 		max.add(shift).add(new THREE.Vector3(100, 300, 100));
-		max.x = max.z = Math.max(max.x, max.z);
-		this.#segments.forEach(s => [s.left1, s.right1].forEach(p => p.add(shift)));
-		return new Track(this.#segments, max);
+		segments.forEach(s => [s.left1, s.right1, s.left2, s.right2].forEach(p => p.add(shift)));
+		return new Track(segments, max);
 	}
 }
 
